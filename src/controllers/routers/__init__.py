35879ff13909506d00fffcd3b9f5692356bee993
hovanvydut <hovanvydut@gmail.com>
2022-06-10 20:02:25 +0700

    add auth by jwt

--- src/controllers/routers/__init__.py
+++ src/controllers/routers/__init__.py
@@ -1,3 +1,4 @@
 from .node_router import node_router
 from .findor_router import findor_router
 from .park_router import park_router
+from .auth_router import authRouter
--- src/controllers/routers/park_router.py
+++ src/controllers/routers/park_router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from controllers.routers.auth_router import auth_login
 from controllers.services import ParkService
 from views import ParkView, ParkUpdate, ParkCreate
 from typing import List
@@ -14,21 +15,21 @@ async def get_park(name:str="", page:int = 1, num_per_page:int=20):
     return list(parks)
 
 @park_router.put("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_park(id: int, data: ParkUpdate):
+async def update_park(id: int, data: ParkUpdate, auth_login = Depends(auth_login)):
     try:
         ParkService.update_park(id, data.dict(exclude_none=True))
     except:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE) 
     
 @park_router.post("/", response_model=ParkView)
-async def create_park(data: ParkCreate):
+async def create_park(data: ParkCreate, auth_login = Depends(auth_login)):
     try:
         return ParkService.create_park(data.dict())
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE) 
     
 @park_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_park(id: int):
+async def delete_park(id: int, auth_login = Depends(auth_login)):
     try:
         return ParkService.delete_park(id)
     except Exception as e:
