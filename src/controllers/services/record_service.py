7f32a0eb4f5402be1918e1cbc6369fe232959ef5
huytuong010101 <huytuong010101@gmail.com>
2022-07-02 08:05:25 +0700

    fix agan

--- src/controllers/services/record_service.py
+++ src/controllers/services/record_service.py
@@ -8,7 +8,7 @@ class RecordService:
     def create_record(
         image: str, park_id: int, time: datetime, num_of_empty_space: int
     ) -> ParkRecord:
-        ParkRecord.delete().where(ParkRecord.park == park_id)
+        ParkRecord.delete().where(ParkRecord.park == park_id).execute()
         record = ParkRecord.create(
             image=image, park=park_id, time=time, num_of_empty_space=num_of_empty_space
         )
